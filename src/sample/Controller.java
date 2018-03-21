757b8dc6a32cac4253542061e15ff2928899f13a
Yuriy Kapelushny <zig.tos@gmail.com>
2018-03-21 14:12:54 +0200

    v.08

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Controller {
 
@@ -12,6 +22,12 @@ public class Controller {
 
     @FXML
     private Button button4;
+
+    @FXML
+    private Pane gridPane;
+
+    @FXML
+    private WebView webView;
 
 
     public void initialize() {
@@ -29,5 +45,32 @@ public class Controller {
     public void handleMouseExit() {
         label.setScaleX(1.0);
         label.setScaleY(1.0);
+    }
+
+    @FXML
+    public void handleClicked() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Save Application File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text", "*.txt"),
+                new FileChooser.ExtensionFilter("PDF", "*.pdf")
+        );
+        File file = chooser.showSaveDialog(gridPane.getScene().getWindow());
+        if (file != null) {
+            System.out.println(file.getPath());
+        } else {
+            System.out.println("Chooser was cancelled");
+        }
+    }
+
+    @FXML
+    public void handleLinkClicked() {
+//        try {
+//            Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+//        } catch (IOException | URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+        WebEngine engine = webView.getEngine();
+        engine.load("http://www.javafx.com");
     }
 }
